e0c979645f26c6b578bafea108685761793c7f23
cleangone <andy_robbins@yahoo.com>
2020-03-21 00:34:12 +0000

    handle login events

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,17 +6,51 @@
     <div v-if="isSignedIn">
       <amplify-sign-out></amplify-sign-out>
     </div>
+
+    <!--<p>isSignedIn: {{isSignedIn}}</p>-->
   </div>
 </template>
 
 <script>
+import { Auth } from 'aws-amplify';
+import { AmplifyEventBus } from 'aws-amplify-vue';
+
 export default {
   name: 'Login',
-  props: {
-    isSignedIn: Boolean
+  data() {
+    return {
+      isSignedIn: Boolean
+    }
   },
   created() {
-    this.signedIn = false;
+    this.isSignedIn = false;
+    this.findUser();
+    
+    AmplifyEventBus.$on('authState', info => {
+      if (info === "signedIn") {
+        // alert("AmplifyEvent - user found")
+        this.findUser();
+      }
+      else {
+        // alert("AmplifyEvent - user not found")
+        this.isSignedIn = false;
+      }
+    });
+
+  },
+  methods: {
+    async findUser() {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        // alert("findUser - user found")
+        this.isSignedIn = true;
+        console.log(user);
+      }
+      catch(err) {
+        // alert("findUser - user not found")
+        this.isSignedIn = false;
+      }
+    }
   }
 }
 </script>
